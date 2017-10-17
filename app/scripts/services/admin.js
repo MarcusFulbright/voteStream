9942a9d8c25b3b8f01ecded8a6ab2eaa33f9603f
Dominic Serrano <dmsrojo@gmail.com>
2017-10-16 21:52:42 -0500

    Admin controller now populating admin page with firebase rooms and times. Correct session indexes are now being sent to be selected. Admin user cannot select sessions for afternoon that have already been selected for morning and vice versa.

--- app/scripts/services/admin.js
+++ app/scripts/services/admin.js
@@ -1,13 +1,13 @@
 'use strict';
 
-app.factory('Admin', function ($http, $q, Constants, SessionListing) {
+app.factory('Admin', function ($http, $q, Constants, SessionListing, Polling) {
 
-  const selectSessions = (indexArr) => {
+  const selectSessions = (indexArr, timeOfDay) => {
     return SessionListing.getAllSessions().then( (data) => {
       const allIndexes = data.map( (sessions, index) => index);
 
       return $q.all(allIndexes.map(i => {
-        let obj = indexArr.includes(i) ? {selected: true} : {selected: false};
+        const obj = indexArr.includes(i) ? {selected: timeOfDay} : {selected: false};
 
         return $http.patch(`${Constants.firebaseUrl}/Sessions/${i}.json`, obj);
       }));
@@ -36,12 +36,18 @@ app.factory('Admin', function ($http, $q, Constants, SessionListing) {
     return $http.put(`${Constants.firebaseUrl}/Schedules/${timeOfDay}.json`, schedule)
     .then(() => {
         //index array has to match index in firebase
-        return selectSessions(scheduleIndexes);
+        // if (timeOfDay === 'Morning') {
+            return selectSessions(scheduleIndexes, timeOfDay);
+        // }
     })
     .then(() => {
         alert(`${timeOfDay} schedule has been updated.`)
+
+        if (timeOfDay === 'Morning') {
+            return Polling.setShowAfternoonTab(true);
+        }
     })
-    .catch(() => {
+    .catch((err) => {
         alert(`Something went wrong with saving the ${timeOfDay} schedule.`);
     });
   };
